efd6a85e666027052d6f65c2aed47d3f36d41f4c
Randy Fong <randyfong@aol.com>
2023-11-04 23:55:17 -0700

    Initial Commit

---
+++ CrimeStopper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrimeStopper
+//
+//  Created by Randy Fong on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrimeStopper/CrimeStopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeStopperApp.swift
+//  CrimeStopper
+//
+//  Created by Randy Fong on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CrimeStopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
